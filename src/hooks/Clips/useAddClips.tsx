ba03819803741050b2d36f5a7894b4237dbc8566
XRikzy <rosendoricardo8@outlook.com>
2024-09-20 13:05:34 -0500

    Eliminate useEffect in useAddClips

--- src/hooks/Clips/useAddClips.tsx
+++ src/hooks/Clips/useAddClips.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import {  useState } from "react";
 import { AddClipValues } from "../../constants/documents";
 import { handleSave } from "../../services/addClips";
 export const useAddClips = () => {
