496f78c7f3308b1c645050e98aeabf07c559bfac
XRikzy <rosendoricardo8@outlook.com>
2024-09-23 18:12:28 -0500

    change fetchdata

--- src/services/getClips.ts
+++ src/services/getClips.ts
@@ -11,10 +11,15 @@ export const fetchData = async (): Promise<Ivideos[]> => {
     const querySnapshot: QuerySnapshot<DocumentData> = await getDocs(
       collection(db, "Clips")
     );
-    return querySnapshot.docs.map((doc) => ({
-      id: doc.id,
-      ...doc.data(),
-    }));
+    return querySnapshot.docs.map((doc) => {
+      const data = doc.data();
+      return {
+        id: doc.id,
+        title: data.title || "",
+        tag: data.tag || [],
+        embed: data.embed || "",
+      } as Ivideos;
+    });
   } catch (error) {
     console.error("Error fetching documents: ", error);
     return [];
